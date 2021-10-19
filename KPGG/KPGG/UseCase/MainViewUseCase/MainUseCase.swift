32be008a9e84831a610483390654237ec3645c4d
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-10-20 00:08:35 +0900

    MainUseCase added

--- KPGG/KPGG/UseCase/MainViewUseCase/MainUseCase.swift
+++ KPGG/KPGG/UseCase/MainViewUseCase/MainUseCase.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+
+protocol MainUseCaseType {
+    func fetchGroupList(_ path: String) -> Observable<[String:[Group]]>
+}
+
+class MainUseCase: MainUseCaseType {
+    
+    var networkService: NetworkService
+    
+    init() {
+        self.networkService = NetworkService()
+    }
+   
+    func fetchGroupList(_ path: String) -> Observable<[String:[Group]]> {
+        return Observable.create { (observer) -> Disposable in
+            self.networkService.sendRequest(path: path, type: GroupResponse.self) { (error, groupList) in
+                if let error = error {
+                    observer.onError(error)
+                }
+                if let groupList = groupList?.group {
+                    observer.onNext(groupList)
+                }
+                observer.onCompleted()
+            }
+            return Disposables.create()
+        }
+    }
+
+}
