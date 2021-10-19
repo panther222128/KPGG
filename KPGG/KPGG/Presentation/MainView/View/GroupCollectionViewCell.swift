ca4ae90b06546948530210827a637b02f1d36560
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-10-19 19:19:28 +0900

    GroupCollectionViewCell configured

--- KPGG/KPGG/Presentation/MainView/View/GroupCollectionViewCell.swift
+++ KPGG/KPGG/Presentation/MainView/View/GroupCollectionViewCell.swift
@@ -9,27 +9,39 @@ import UIKit
 
 class GroupCollectionViewCell: UICollectionViewCell {
     
+    var groupImage: UIImageView!
     var groupName: UILabel!
     
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         setUpCell()
     }
     
-    override init(frame: CGRect) {
-        super.init(frame: .zero)
-        setUpCell()
+    required init?(coder: NSCoder) {
+        fatalError("not implemented")
     }
     
     func setUpCell() {
         groupName = UILabel()
+        groupImage = UIImageView()
+        
         contentView.addSubview(groupName)
+        contentView.addSubview(groupImage)
+        
         groupName.translatesAutoresizingMaskIntoConstraints = false
         groupName.textAlignment = .center
-        groupName.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
-        groupName.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
-        groupName.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
-        groupName.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: 5).isActive = true
+        groupName.topAnchor.constraint(equalTo: groupImage.bottomAnchor, constant: 5).isActive = true
+        groupName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5).isActive = true
+        groupName.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5).isActive = true
+        groupName.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5).isActive = true
+        
+        groupImage.translatesAutoresizingMaskIntoConstraints = false
+        groupImage.contentMode = .scaleAspectFit
+        groupImage.backgroundColor = .systemBlue
+        groupImage.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5).isActive = true
+        groupImage.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5).isActive = true
+        groupImage.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5).isActive = true
+        groupImage.bottomAnchor.constraint(equalTo: groupName.topAnchor, constant: -5).isActive = true
     }
     
 }
