adbfd337f8e03c5cb8df3a9848b259e5a0463e26
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-10-20 00:07:24 +0900

    NetworkService fixed

--- KPGG/KPGG/NetworkService/NetworkService.swift
+++ KPGG/KPGG/NetworkService/NetworkService.swift
@@ -19,7 +19,7 @@ class NetworkService {
     func sendRequest<T: Decodable>(path: String, type: T.Type, completion: @escaping ((Error?, T?) -> Void)) {
         let url = endPoint.url(with: path)
         AF.request(url!, method: .get)
-            .validate(statusCode: 200..<300)
+            .validate(statusCode: 200..<600)
             .responseDecodable(of: type) { response in
                 if let error = response.error {
                     print(error)
@@ -29,7 +29,6 @@ class NetworkService {
                     return completion(nil, list)
                 }
             }
-        
     }
     
 }
