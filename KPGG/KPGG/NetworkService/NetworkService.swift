58a39e38e18244878c50c9877003a3feba4ded68
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-10-19 19:17:25 +0900

    NetworkService added

--- KPGG/KPGG/NetworkService/NetworkService.swift
+++ KPGG/KPGG/NetworkService/NetworkService.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import Alamofire
+
+class NetworkService {
+    
+    private var endPoint: Endpoint
+    
+    init() {
+        self.endPoint = Endpoint()
+    }
+    
+    func sendRequest<T: Decodable>(path: String, type: T.Type, completion: @escaping ((Error?, T?) -> Void)) {
+        let url = endPoint.url(with: path)
+        AF.request(url!, method: .get)
+            .validate(statusCode: 200..<300)
+            .responseDecodable(of: type) { response in
+                if let error = response.error {
+                    print(error)
+                    return completion(error, nil)
+                }
+                if let list = response.value {
+                    return completion(nil, list)
+                }
+            }
+        
+    }
+    
+}
