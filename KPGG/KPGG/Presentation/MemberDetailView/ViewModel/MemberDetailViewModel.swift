d0e034a52c50634f0ca256271a22a40b255853fd
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-10-20 23:20:07 +0900

    MemberDetailViewModel added

--- KPGG/KPGG/Presentation/MemberDetailView/ViewModel/MemberDetailViewModel.swift
+++ KPGG/KPGG/Presentation/MemberDetailView/ViewModel/MemberDetailViewModel.swift
@@ -6,3 +6,76 @@
 //
 
 import Foundation
+
+protocol MemberDetailViewModelType {
+    func activityName() -> String
+    func name() -> String
+    func birth() -> String
+    func mbti() -> String
+    func bloodType() -> String
+    func mainImage() -> String
+    func isPreviousMember() -> String
+}
+
+class MemberDetailViewModel: MemberDetailViewModelType {
+    
+    var member: Member?
+    
+    init(member: Member) {
+        self.member = member
+    }
+    
+    func activityName() -> String {
+        guard let member = member else {
+            return ""
+        }
+        return member.activityname
+    }
+    
+    func name() -> String {
+        guard let member = member else {
+            return ""
+        }
+        return member.name
+    }
+    
+    func birth() -> String {
+        guard let member = member else {
+            return ""
+        }
+        return member.birth
+    }
+    
+    func mbti() -> String {
+        guard let member = member else {
+            return ""
+        }
+        return member.mbti
+    }
+    
+    func bloodType() -> String {
+        guard let member = member else {
+            return ""
+        }
+        return member.bloodtype
+    }
+    
+    func mainImage() -> String {
+        guard let member = member else {
+            return ""
+        }
+        return member.mainimage
+    }
+    
+    func isPreviousMember() -> String {
+        guard let member = member else {
+            return ""
+        }
+        if member.ispreviousmember {
+            return "탈퇴"
+        } else {
+            return "활동"
+        }
+    }
+    
+}
