739e5bba8fd9f87882bc6a5d74b836d86c1ab4cd
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-10-20 20:56:32 +0900

    GroupMemberUseCase added

--- KPGG/KPGG/UseCase/GroupMemberUseCase/GroupMemberUseCase.swift
+++ KPGG/KPGG/UseCase/GroupMemberUseCase/GroupMemberUseCase.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+protocol GroupMemberUseCaseType {
+    func fetchMemberList(_ path: String) -> Observable<[String:[Member]]>
+}
+
+class GroupMemberUseCase: GroupMemberUseCaseType {
+    
+    var networkService: NetworkService
+    
+    init() {
+        self.networkService = NetworkService()
+    }
+   
+    func fetchMemberList(_ path: String) -> Observable<[String:[Member]]> {
+        return Observable.create { (observer) -> Disposable in
+            self.networkService.sendRequest(path: path, type: MemberResponse.self) { (error, memberList) in
+                if let error = error {
+                    observer.onError(error)
+                }
+                if let memberList = memberList?.groupname {
+                    observer.onNext(memberList)
+                }
+                observer.onCompleted()
+            }
+            return Disposables.create()
+        }
+    }
+
+}
