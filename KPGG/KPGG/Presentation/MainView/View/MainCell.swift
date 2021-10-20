1b731cf66ef68b4a3f01bdb262e5b3619e465e7d
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-10-20 14:44:36 +0900

    Cell layout fixed

--- KPGG/KPGG/Presentation/MainView/View/MainCell.swift
+++ KPGG/KPGG/Presentation/MainView/View/MainCell.swift
@@ -10,7 +10,6 @@ import UIKit
 class MainCell: UICollectionViewCell {
     
     var groupImage: UIImageView!
-    var groupName: UILabel!
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -24,22 +23,12 @@ class MainCell: UICollectionViewCell {
     
     func setUpCell() {
         groupImage = UIImageView()
-        groupName = UILabel()
         contentView.addSubview(groupImage)
-        contentView.addSubview(groupName)
-        
         groupImage.translatesAutoresizingMaskIntoConstraints = false
         groupImage.contentMode = .scaleAspectFit
         groupImage.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor).isActive = true
         groupImage.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor).isActive = true
         groupImage.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor).isActive = true
         groupImage.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor).isActive = true
-        
-        groupName.translatesAutoresizingMaskIntoConstraints = false
-        groupName.textAlignment = .left
-        groupName.textColor = .white
-        groupName.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5).isActive = true
-        groupName.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5).isActive = true
-        groupName.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -5).isActive = true
     }
 }
