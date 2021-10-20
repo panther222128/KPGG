5f66c9111de58ebc67a6ec9b7ca6e27b90d75959
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-10-20 20:51:57 +0900

    GroupMemberViewController added

--- KPGG/KPGG/Presentation/GroupMemberView/Controller/GroupMemberViewController.swift
+++ KPGG/KPGG/Presentation/GroupMemberView/Controller/GroupMemberViewController.swift
@@ -6,24 +6,84 @@
 //
 
 import UIKit
+import RxSwift
 
 class GroupMemberViewController: UIViewController {
-
+    
+    @IBOutlet weak var groupMember: UITableView!
+    @IBOutlet weak var representativeSongButton: UIButton!
+    
+    private var groupMemberViewModel: GroupMemberViewModelType?
+    private var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        viewConfiguration()
+        configureGroupMember()
+        configureRepresentativeSongButton()
+        subscribe()
+        fetchMembers()
+        groupMember.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func subscribe()  {
+        self.groupMemberViewModel?.membersSubject().subscribe(onNext:{ [weak self] _ in
+            self?.groupMember.reloadData()
+        }).disposed(by: disposeBag)
     }
-    */
+    
+    private func fetchMembers() {
+        self.groupMemberViewModel?.fetch().subscribe { [weak self] members in
+            self?.groupMemberViewModel?.configureMembers(members)
+        } onError: { [weak self] error in
+            let error = error.localizedDescription
+            self?.makeErrorAlert(error: error)
+        }.disposed(by: disposeBag)
+    }
+    
+    private func makeErrorAlert(error: String) {
+        let alert = UIAlertController(title: "네트워크 에러", message: "\(error)", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+    private func viewConfiguration() {
+        self.view.backgroundColor = .black
+    }
+    
+    private func configureGroupMember() {
+        groupMember.backgroundColor = .black
+    }
+    
+    private func configureRepresentativeSongButton() {
+        representativeSongButton.titleLabel?.text = "대표곡 듣기"
+    }
+    
+    func showGroupMemberViewController(with viewModel: GroupMemberViewModelType) {
+        self.groupMemberViewModel = viewModel
+    }
+    
+}
 
+extension GroupMemberViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let count = self.groupMemberViewModel?.membersCount() else { return 0 }
+        return count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MemberCell", for: indexPath) as? GroupMemberViewCell else { return UITableViewCell() }
+        let imageUrl = URL(string: groupMemberViewModel?.member()?[indexPath.row].mainimage ?? "")
+        var data = Data()
+        do {
+            data = try Data(contentsOf: imageUrl!)
+        } catch {
+            
+        }
+        let memberActivityName = groupMemberViewModel?.member()?[indexPath.row].activityname ?? ""
+        cell.configureCell(memberActivityName: memberActivityName, imageData: data)
+        return cell
+    }
+    
 }
