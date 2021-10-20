7e3533f5001ae2f4540a056560f7936af81bfcc7
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-10-20 14:44:18 +0900

    Description fixed

--- KPGG/KPGG/Presentation/MainView/View/SectionEnum.swift
+++ KPGG/KPGG/Presentation/MainView/View/SectionEnum.swift
@@ -12,14 +12,14 @@ enum Section: Int, Hashable, CaseIterable, CustomStringConvertible {
     
     var description: String {
         switch self {
-        case .zero: return "Zero"
-        case .first: return "First"
-        case .second: return "Second"
-        case .third: return "Third"
-        case .fourth: return "Fourth"
-        case .fifth: return "Fifth"
-        case .sixth: return "Sixth"
-        case .seventh: return "Seventh"
+        case .zero: return "zero"
+        case .first: return "first"
+        case .second: return "second"
+        case .third: return "third"
+        case .fourth: return "fourth"
+        case .fifth: return "fifth"
+        case .sixth: return "sixth"
+        case .seventh: return "seventh"
         }
     }
 }
