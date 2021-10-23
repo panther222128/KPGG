9e04489a45d7f47803f17743064b346964b2c2c7
Jun Ho JANG <junhojang@Junui-MacBookPro.local>
2021-10-23 21:14:09 +0900

    Separate view object

--- KPGG/KPGG/Presentation/MainView/Controller/MainViewController.swift
+++ KPGG/KPGG/Presentation/MainView/Controller/MainViewController.swift
@@ -11,7 +11,7 @@ import Kingfisher
 
 class MainViewController: UIViewController {
     
-    @IBOutlet weak var section: UICollectionView!
+    @IBOutlet weak var sectionView: SectionView!
     
     private var mainViewModel: MainViewModelType?
     private var dataSource: UICollectionViewDiffableDataSource<Section, Group>!
@@ -23,8 +23,8 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         self.showMainViewController(with: MainViewModel())
         fetchGroups(path: "")
-        configureHierarchy()
-        section.delegate = self
+        configure()
+        sectionView.delegate = self
         subscribe()
         configureNavigation()
     }
@@ -51,45 +51,8 @@ class MainViewController: UIViewController {
         self.present(alert, animated: true)
     }
     
-    private func configureHierarchy() {
-        section.collectionViewLayout = createLayout()
-        section.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        section.backgroundColor = .black
+    private func configure() {
         self.view.backgroundColor = .black
-    }
-    
-    private func createLayout() -> UICollectionViewLayout {
-        let sectionProvider = { (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
-            guard let sectionKind = Section(rawValue: sectionIndex) else { return nil }
-            let section: NSCollectionLayoutSection
-            switch sectionKind {
-            case .zero:
-                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-                let item = NSCollectionLayoutItem(layoutSize: itemSize)
-                item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
-                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1))
-                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-                section = NSCollectionLayoutSection(group: group)
-                section.interGroupSpacing = 0
-                section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
-            default:
-                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-                let item = NSCollectionLayoutItem(layoutSize: itemSize)
-                item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
-                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .fractionalWidth(0.35))
-                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-                let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .estimated(35)),elementKind: MainViewController.sectionHeaderElementKind, alignment: .top)
-                sectionHeader.pinToVisibleBounds = true
-                sectionHeader.zIndex = 2
-                section = NSCollectionLayoutSection(group: group)
-                section.boundarySupplementaryItems = [sectionHeader]
-                section.interGroupSpacing = 2
-                section.orthogonalScrollingBehavior = .continuousGroupLeadingBoundary
-                section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 10)
-            }
-            return section
-        }
-        return UICollectionViewCompositionalLayout(sectionProvider: sectionProvider)
     }
     
     private func setupDiffableDataSource() {
@@ -130,7 +93,7 @@ class MainViewController: UIViewController {
             cell.backgroundConfiguration = background
         }
         
-        dataSource = UICollectionViewDiffableDataSource<Section, Group>(collectionView: section) {
+        dataSource = UICollectionViewDiffableDataSource<Section, Group>(collectionView: sectionView) {
             (collectionView, indexPath, group) -> UICollectionViewCell? in
             guard let section = Section(rawValue: indexPath.section) else { fatalError("Unknown section") }
             switch section {
@@ -142,7 +105,7 @@ class MainViewController: UIViewController {
         }
         
         dataSource.supplementaryViewProvider = { (view, kind, index) in
-            return self.section.dequeueConfiguredReusableSupplementary(using: headerRegistration, for: index)
+            return self.sectionView.dequeueConfiguredReusableSupplementary(using: headerRegistration, for: index)
         }
         
     }
@@ -175,7 +138,7 @@ class MainViewController: UIViewController {
         self.navigationController?.setStatusBar(backgroundColor: .clear)
 
         if #available(iOS 11.0, *) {
-            self.section.contentInsetAdjustmentBehavior = .never
+            self.sectionView.contentInsetAdjustmentBehavior = .never
         } else {
             automaticallyAdjustsScrollViewInsets = false
         }
@@ -196,11 +159,11 @@ extension MainViewController: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let groupName = self.dataSource.itemIdentifier(for: indexPath)?.groupname else {
-            section.deselectItem(at: indexPath, animated: true)
+            sectionView.deselectItem(at: indexPath, animated: true)
             return
         }
         guard let groupHitSong = self.dataSource.itemIdentifier(for: indexPath)?.hitsong else {
-            section.deselectItem(at: indexPath, animated: true)
+            sectionView.deselectItem(at: indexPath, animated: true)
             return
         }
         guard let groupMemberViewController = self.storyboard?.instantiateViewController(withIdentifier: "GroupMember") as? GroupMemberViewController else { return }
